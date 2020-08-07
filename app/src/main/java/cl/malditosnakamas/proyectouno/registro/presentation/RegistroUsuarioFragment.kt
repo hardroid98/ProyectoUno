bcd31ae41af7500188c8e3e0771497984ad01734
Harttyn Arce <harttyn.arce@gmail.com>
2020-08-06 21:55:41 -0400

    Se agrega layout

---
+++ app/src/main/java/cl/malditosnakamas/proyectouno/registro/presentation/RegistroUsuarioFragment.kt
@@ -0,0 +1,15 @@
+package cl.malditosnakamas.proyectouno.registro.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cl.malditosnakamas.proyectouno.R
+
+
+class RegistroUsuarioFragment : Fragment(R.layout.fragment_registro_usuario) {
+
+
+
+}
