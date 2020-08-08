449fa0baee778ce0f692094affca0b382ee7e0b8
vialepau <vialepau@gmail.com>
2020-08-07 21:23:37 -0400

    creacion carpetas CA

---
+++ app/src/main/java/cl/malditosnakamas/proyectouno/login/presentation/LoginFragment.kt
@@ -0,0 +1,10 @@
+package cl.malditosnakamas.proyectouno.login.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import cl.malditosnakamas.proyectouno.R
+
+class LoginFragment : Fragment(R.layout.fragment_login)
