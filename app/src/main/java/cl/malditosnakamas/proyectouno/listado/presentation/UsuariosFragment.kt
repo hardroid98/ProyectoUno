3e2b32146862bcc131e2a2b462da51a8eafeedc4
Geraldine <geraldine.b.09@gmail.com>
2020-08-07 21:56:10 -0400

    Cambios en usuarios fragment

---
+++ app/src/main/java/cl/malditosnakamas/proyectouno/listado/presentation/UsuariosFragment.kt
@@ -0,0 +1,6 @@
+package cl.malditosnakamas.proyectouno.listado.presentation
+
+import androidx.fragment.app.Fragment
+import cl.malditosnakamas.proyectouno.R
+
+class UsuariosFragment : Fragment(R.layout.fragment_usuarios)
