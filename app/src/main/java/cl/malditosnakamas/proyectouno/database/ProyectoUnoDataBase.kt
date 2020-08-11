2ce29f4d2f469cd5366022b4bb9af2ac3bfb4008
Harttyn Arce <harttyn.arce@gmail.com>
2020-08-11 19:18:47 -0400

    Se agrega local repository

--- app/src/main/java/cl/malditosnakamas/proyectouno/database/ProyectoUnoDataBase.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/database/ProyectoUnoDataBase.kt
@@ -2,10 +2,12 @@ package cl.malditosnakamas.proyectouno.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import cl.malditosnakamas.proyectouno.login.data.local.database.LoginDao
 import cl.malditosnakamas.proyectouno.registro.data.local.database.RegistroDao
-import cl.malditosnakamas.persistenciadedatos.personajes.data.local.database.RegistroEntity
+import cl.malditosnakamas.proyectouno.registro.data.local.database.RegistroEntity
 
 @Database(entities = [RegistroEntity::class], version = 1)
 abstract class ProyectoUnoDataBase : RoomDatabase() {
     abstract fun registroDao(): RegistroDao
+    abstract fun loginDao(): LoginDao
 }
--- app/src/main/java/cl/malditosnakamas/proyectouno/login/domain/LoginUseCase.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/login/domain/LoginUseCase.kt
@@ -1,10 +1,7 @@
 package cl.malditosnakamas.proyectouno.login.domain
 
-import cl.malditosnakamas.proyectouno.registro.domain.Registro
-import cl.malditosnakamas.proyectouno.registro.domain.RegistroRepository
-
-class LoginUseCase (
-    private val repository: LoginRepository){
-
-        fun execute(login: Login) = repository.login(login)
-    }
+class LoginUseCase(
+    private val repository: LoginRepository
+) {
+    fun execute(login: Login) = repository.login(login)
+}
