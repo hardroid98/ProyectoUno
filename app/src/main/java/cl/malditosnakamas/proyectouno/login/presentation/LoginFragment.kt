4e810063a36ec9f50440a148ce1050733928cffc
Harttyn Arce <harttyn.arce@gmail.com>
2020-08-11 19:28:48 -0400

    Se prepara update de develop

--- app/src/main/java/cl/malditosnakamas/proyectouno/login/presentation/LoginFragment.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/login/presentation/LoginFragment.kt
@@ -6,5 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import cl.malditosnakamas.proyectouno.R
+import cl.malditosnakamas.proyectouno.databinding.FragmentLoginBinding
 
-class LoginFragment : Fragment(R.layout.fragment_login)
+class LoginFragment : Fragment(R.layout.fragment_login){
+
+    lateinit var binding: FragmentLoginBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupDependencies()
+        bindView(view)
+        setupListeners()
+    }
+
+    private fun setupDependencies() {
+
+    }
+
+    private fun bindView(view: View) {
+        binding = FragmentLoginBinding.bind(view)
+    }
+
+    private fun setupListeners() {
+        binding.btnIngresar.setOnClickListener {
+            doClickLogin()
+        }
+    }
+
+    private fun doClickLogin() {
+        if(isValidInputValues()){
+
+        }
+    }
+
+    private fun isValidInputValues(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
