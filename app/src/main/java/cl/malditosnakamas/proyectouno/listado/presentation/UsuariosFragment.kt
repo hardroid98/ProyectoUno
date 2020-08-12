b36f71ea9d83136bb769d66d58391a0091a75f96
Julio Delgado <correodejulioc@gmail.com>
2020-08-11 20:09:17 -0400

    creacion de funciones en el fragment

--- app/src/main/java/cl/malditosnakamas/proyectouno/listado/presentation/UsuariosFragment.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/listado/presentation/UsuariosFragment.kt
@@ -1,15 +1,21 @@
 package cl.malditosnakamas.proyectouno.listado.presentation
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import cl.malditosnakamas.proyectouno.R
 import cl.malditosnakamas.proyectouno.databinding.FragmentUsuariosBinding
+import cl.malditosnakamas.proyectouno.listado.data.local.LocalUsuariosRepository
 import cl.malditosnakamas.proyectouno.listado.data.local.UsuariosMapper
 import cl.malditosnakamas.proyectouno.listado.domain.ObtenerUsuariosUseCase
 import cl.malditosnakamas.proyectouno.listado.domain.UsuariosRepository
 import cl.malditosnakamas.proyectouno.listado.domain.model.Usuario
+import cl.malditosnakamas.proyectouno.listado.domain.model.Usuarios
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class UsuariosFragment: Fragment(R.layout.fragment_usuarios),
     ItemClickListener {
@@ -22,14 +28,20 @@ class UsuariosFragment: Fragment(R.layout.fragment_usuarios),
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupDependencies()
+        bindView(view)
         setupRecyclerView()
         setupUseCase()
         binding = FragmentUsuariosBinding.bind(view)
 
     }
 
-    private fun setupUseCase() {
-        // TODO: AIURAAAAA!!!
+    private fun setupDependencies() {
+        TODO("Not yet implemented")
+    }
+
+    private fun bindView(view: View) {
+        binding = FragmentUsuariosBinding.bind(view)
     }
 
     private fun setupRecyclerView() {
@@ -43,8 +55,34 @@ class UsuariosFragment: Fragment(R.layout.fragment_usuarios),
         }
     }
 
-    override fun onItemClick(usuario: Usuario) {
-        // TODO: AIURAAAAA!!!
+    @SuppressLint("CheckResult")
+    private fun setupUseCase() {
+        repository = LocalUsuariosRepository(requireContext(), mapper)
+        useCase = ObtenerUsuariosUseCase(repository)
+        useCase.excecute()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(
+                {result -> handleReult(result)},
+                {error -> handleError(error)}
+            )
     }
 
+    private fun handleError(error: Throwable) {
+        Toast.makeText(requireContext(),
+            "Error {${error.message}}",
+            Toast.LENGTH_LONG)
+            .show()
+    }
+
+    private fun handleReult(result: Usuarios) {
+        val adapter = UsuariosAdapter(result.listaUsuarios)
+        binding.rvUsuarios.adapter = usuariosAdapter
+        }
+
+    override fun onItemClick(usuario: Usuario) {
+        binding.rvUsuarios.setOnClickListener {
+            // Navigation.findNavController(getView()).navigate(R.id.action_usuariosFragment_to_detallesFragment);        }
+        }
+    }
 }
