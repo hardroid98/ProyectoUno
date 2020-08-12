1488a3dfd58babbbbfbc749c321d024675246f4b
Julio Delgado <correodejulioc@gmail.com>
2020-08-11 22:27:04 -0400

    cambios en el DAO y entity

--- app/src/main/java/cl/malditosnakamas/proyectouno/listado/data/local/database/UsuarioEntity.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/listado/data/local/database/UsuarioEntity.kt
@@ -1,14 +1,7 @@
 package cl.malditosnakamas.proyectouno.listado.data.local.database
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "usuarios")
 data class UsuarioEntity (
-    @ColumnInfo(name = "nombre") val nombre: String,
-    @ColumnInfo(name = "email") val email: String,
-    @ColumnInfo(name = "rut") val rut: String
-) {
-    @PrimaryKey(autoGenerate = true) var id: Int = 0
-}
+    val nombre: String,
+    val email: String,
+    val rut: String
+)
--- app/src/main/java/cl/malditosnakamas/proyectouno/listado/data/local/database/UsuariosDao.kt
+++ app/src/main/java/cl/malditosnakamas/proyectouno/listado/data/local/database/UsuariosDao.kt
@@ -2,11 +2,12 @@ package cl.malditosnakamas.proyectouno.listado.data.local.database
 
 import androidx.room.Dao
 import androidx.room.Query
+import cl.malditosnakamas.persistenciadedatos.personajes.data.local.database.RegistroEntity
 import io.reactivex.Single
 
 @Dao
 interface UsuariosDao {
 
-    @Query("SELECT * FROM usuarios")
+    @Query("SELECT nombre, rut, email FROM registro")
     fun obtenerDatos(): Single<List<UsuarioEntity>>
 }
